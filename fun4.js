8db237966e4975d41ca319a0aafd437446a336a7
Aparna10-D <156919386+Aparna10-D@users.noreply.github.com>
2024-06-18 20:26:36 +0530

    Add files via upload

---
+++ fun4.js
@@ -0,0 +1,11 @@
+
+let showProducts=()=>{
+    return ["Laptop","Mobile phone","Bluetooth speakers","Smart watch"]
+}
+let products = showProducts();
+for(i in products){
+    console.log(products[i]);
+
+}
+
+
